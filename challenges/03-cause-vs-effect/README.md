6e7cd430654a45145a83431498c48f79021f3baa
Felipe Araujo <devaraujofelipe@gmail.com>
2024-02-12 15:59:43 +0100

    feat: complete challenge 03-cause-vs-effect

---
+++ challenges/03-cause-vs-effect/README.md
@@ -0,0 +1,11 @@
+# Cause vs Effect
+
+In this challenge, you will put into practice everything you just learned in the `Cause vs Effect` lesson of the **Clean Code** course.
+
+# About the challenge
+
+The code in which you will practice these concepts contains the logic of a React component that makes a call to a function that returns user data.
+
+While the user data is not returned, the `Loading` behaviour is displayed on the screen so that it does not remain blank.
+
+Your goal in this code is not to fix or implement its functionality, as it already works, but you should rename variables or even object attributes according to what was learned, giving meaning to the variables according to the rules above.
---
+++ challenges/03-cause-vs-effect/solution.tsx
@@ -0,0 +1,48 @@
+// Cause vs Effect
+import { useEffect, useState } from 'react';
+
+interface User {
+  name: string;
+  github: string;
+}
+
+function fetchUser() {
+  return {
+    data: {
+      user: {
+        name: 'Joseph Oliveira',
+        github: 'https://github.com/josepholiveira',
+      },
+    },
+  };
+}
+
+export function UserProfile() {
+  const [isFetchingUser, setIsFetchingUser] = useState(false);
+  const [userData, setUserData] = useState<User>();
+
+  useEffect(() => {
+    function loadUser() {
+      setIsFetchingUser(true);
+
+      const fetchUserResponse = fetchUser();
+
+      setUserData(fetchUserResponse.data.user);
+
+      setIsFetchingUser(false);
+    }
+
+    loadUser();
+  });
+
+  if (isFetchingUser) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={`${userData?.github}.png`} alt="" />
+      <a href={userData?.github}>{userData?.name}</a>
+    </div>
+  );
+}
